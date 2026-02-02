6f6e997adfb48939a64a627a3683d4886ef858e2
LALIE LARABI <lalielarabi@gmail.com>
2026-02-02 10:56:57 +0100

    green_blackwhite_mirror

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,78 @@
 #include <sil/sil.hpp>
 
+void green(sil::Image& image)
+{
+    for (int y = 0; y < image.height(); ++y)
+    {
+        for (int x = 0; x < image.width(); ++x)
+        {
+            image.pixel(x,y).r = 0.f;
+            image.pixel(x,y).b = 0.f;
+        }
+    }
+}
+
+void blackwhite(sil::Image& image){
+    for (int y = 0; y < image.height(); ++y)
+    {
+        for (int x = 0; x < image.width(); ++x)
+        {
+            float gray = (image.pixel(x,y).r + image.pixel(x,y).g + image.pixel(x,y).b) / 3.0f;
+            image.pixel(x,y).r = gray;
+            image.pixel(x,y).g = gray;
+            image.pixel(x,y).b = gray;
+        }
+    }
+}
+
+void blackwhite1(sil::Image& image){
+    for (int y = 0; y < image.height(); ++y)
+    {
+        for (int x = 0; x < image.width(); ++x)
+        {
+            float gray = (image.pixel(x,y).r*0.2126f + image.pixel(x,y).g*0.7152f + image.pixel(x,y).b*0.0722f);
+            image.pixel(x,y).r = gray;
+            image.pixel(x,y).g = gray;
+            image.pixel(x,y).b = gray;
+        }
+    }
+}
+
+void mirror(sil::Image& image){
+    sil::Image copy = image;
+    for (int y = 0; y < image.height(); ++y)
+    {
+        for (int x = 0; x < image.width(); ++x)
+        {
+            image.pixel(x,y)=copy.pixel(image.width() - (x+1), y);
+        }
+    }
+}
+
 int main()
 {
+    { 
     sil::Image image{"images/logo.png"};
-    // TODO: modifier l'image
-    image.save("output/pouet.png");
+    green(image);
+    image.save("output/green_logo.png");
+    }
+
+    {
+    sil::Image image{"images/logo.png"};
+    blackwhite(image);
+    image.save("output/blackwhite_logo.png");
+    }
+
+    {
+    sil::Image image{"images/logo.png"};
+    blackwhite1(image);
+    image.save("output/blackwhite1_logo.png");
+    }
+
+    {
+    sil::Image image{"images/logo.png"};
+    mirror(image);
+    image.save("output/mirror_logo.png");
+    }
+    
 }
